681406932b803363969504d9d9e2bf607c85c907
Samet <sameterayerdm@gmail.com>
2019-05-06 06:49:30 +0300

    user sign up

--- src/main/java/book/bookplatform/user/controller/UserController.java
+++ src/main/java/book/bookplatform/user/controller/UserController.java
@@ -16,7 +16,6 @@ import javax.validation.Valid;
 @RestController
 @AllArgsConstructor
 @RequestMapping("api")
-@CrossOrigin("*")
 public class UserController {
 
 
--- src/main/java/book/bookplatform/user/model/UserSignUpRequestModel.java
+++ src/main/java/book/bookplatform/user/model/UserSignUpRequestModel.java
@@ -20,20 +20,20 @@ public class UserSignUpRequestModel implements Serializable {
 
     @Size(min = 3, max = 35, message = "Lütfen Uygun Bir İsim Giriniz!")
     @Pattern(regexp = "^(([A-za-z ğüşöçİĞÜŞÖÇ]+[\\s]{1}[A-za-z ğüşöçİĞÜŞÖÇ]+)|([A-Za-z]+))$", message = "Lütfen Uygun Bir İsim Giriniz")
-    @NotNull(message = "isim null olamaz")
+    @NotNull(message = "Lütfen Uygun Bir İsim Giriniz")
     private String name;
 
     @Size(min = 3, max = 25)
     @Pattern(regexp = "^[A-z ğüşöçİĞÜŞÖÇ](?!\\s)[a-z ğüşöçİĞÜŞÖÇ]*((\\W)?[A-Z İĞÜŞÖÇ][a-z ğüşöç]+)*$", message = "Uygun bir soyad giriniz")
-    @NotNull(message = "soyad null olamaz")
+    @NotNull(message = "Uygun bir soyad giriniz")
     private String surname;
 
-    @Email(message = "geçerli bir e-mail adresi giriniz")
-    @NotNull(message = "e mail null olamaz")
+    @Email(message = "Geçerli bir e-mail adresi giriniz")
+    @NotNull(message = "Geçerli bir e-mail adresi giriniz")
     private String email;
 
     @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$", message = "Paralonız en az bir rakam  büyük harf ve özel karakter içermelidir")
-    @NotNull(message = "parala null olamaz")
+    @NotNull(message = "Paralonız en az bir rakam  büyük harf ve özel karakter içermelidir")
     private String password;
 
     @NotNull(message = "parola doğrulama null olamaz")
